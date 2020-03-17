9005d8dc68af095d78b8802938f22b438a707cd0
Thapelo Moeti <benzitto@gmail.com>
2020-03-17 21:26:07 +0200

    added post author

--- parts/excerpt-gallery.php
+++ parts/excerpt-gallery.php
@@ -3,6 +3,7 @@
 <article id="post-<?php the_id(); ?>" <?php post_class('post-excerpt'); ?>>
     <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <div class="post-meta">
+        <span class="post-author"><i class="fas fa-user"></i> <?php the_author(); ?></span>
         <span class="post-date"><i class="fas fa-calendar"></i> <?php tdt_one_the_date(); ?></span>
         <span class="post-categories"><i class="fas fa-folder"></i><?php tdt_one_the_category(); ?></span>
     </div>
