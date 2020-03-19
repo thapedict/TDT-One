e21814521a3496e2e3bfa4e0191ca4a4c7c77773
Thapelo Moeti <benzitto@gmail.com>
2020-03-19 14:57:23 +0200

    added link to post

--- parts/excerpt-quote.php
+++ parts/excerpt-quote.php
@@ -5,11 +5,13 @@
     <meta content="<?php tdt_one_the_date(); ?>" />
     <meta content="<?php the_permalink(); ?>" />
     <div class="post-content">
-    <?php
-    $content = get_the_content();
-            
-    print tdt_one_wrap_with_blockquote($content);
-    ?>
+        <a href="<?php echo esc_url( get_the_permalink() ); ?>">
+            <?php
+            $content = get_the_content();
+                    
+            print tdt_one_wrap_with_blockquote($content);
+            ?>
+        </a>
     </div>
 </article>
 
--- parts/excerpt-status.php
+++ parts/excerpt-status.php
@@ -2,7 +2,9 @@
 <!-- excerpt-status -->
 <article id="post-<?php the_id(); ?>" <?php post_class(); ?>>
     <div class="post-content">
-    <?php the_content(); ?>
+        <a href="<?php echo esc_url( get_the_permalink() ); ?>">
+            <?php the_content(); ?>
+        </a>
     </div>
     <div class="post-meta align-center">
         <span class="post-date"><i class="fas fa-calendar"></i> <?php tdt_one_the_date(); ?></span>
